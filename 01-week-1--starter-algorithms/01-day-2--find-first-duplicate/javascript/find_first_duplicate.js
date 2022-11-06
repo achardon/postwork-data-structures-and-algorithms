f29b8f3082d2ba98d80c518793591a9993fc8c6b
Alessandra Chardon <achardon@ucla.edu>
2022-11-05 22:59:43 -0400

    solved first duplicate

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,19 +1,39 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let duplicate = false
+  //iterate through array
+  for (let i = 0; i < arr.length - 1; i++) {
+    //for each number, check to see if number exists before
+    const currentNumber = arr[i]
+    indexOfDuplicate = arr.indexOf(currentNumber)
+    //if exists, print number
+    if(indexOfDuplicate < i) {
+      console.log(arr[indexOfDuplicate])
+      duplicate = true
+      break;
+    }
+    //if there is never a duplicate, print -1
+  }
+  if (duplicate === false) {
+    console.log(-1)
+  }
 }
 
-if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+findFirstDuplicate([2, 1, 3, 3, 2]);
+findFirstDuplicate([1, 2, 3, 4]);
+findFirstDuplicate([5,6,7,8,7,6,5,4])
 
-  console.log("");
+// if (require.main === module) {
+//   // add your own tests in here
+//   console.log("Expecting: 3");
+//   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
-  console.log("Expecting: -1");
-  console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
-}
+//   console.log("");
 
-module.exports = findFirstDuplicate;
+//   console.log("Expecting: -1");
+//   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+// }
+
+// module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
