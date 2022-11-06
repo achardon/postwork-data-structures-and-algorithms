ccb5b2b4d7a77247c8508a9c06923f1820621fd4
Alessandra Chardon <achardon@ucla.edu>
2022-11-05 22:39:58 -0400

    solved

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,19 +1,40 @@
 function reverseString(str) {
-  // type your code here
+
+  //split string into array of characters
+  const array = str.split("")
+  console.log(array)
+
+  //find length of array
+  length = array.length
+  console.log(length)
+
+  //iterate through array, add last character as first character of new string
+
+  newString = ""
+  for (let i = length - 1; i >= 0; i--) {
+    newString = newString + array[i];
+  }
+
+  console.log(newString)
+  
+  //return string
+  return newString
 }
 
-if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 'ih'");
-  console.log("=>", reverseString("hi"));
+reverseString("alessandra");
 
-  console.log("");
+// if (require.main === module) {
+//   // add your own tests in here
+//   console.log("Expecting: 'ih'");
+//   console.log("=>", reverseString("hi"));
 
-  console.log("Expecting: 'ybabtac'");
-  console.log("=>", reverseString("catbaby"));
-}
+//   console.log("");
 
-module.exports = reverseString;
+//   console.log("Expecting: 'ybabtac'");
+//   console.log("=>", reverseString("catbaby"));
+// }
+
+// module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
